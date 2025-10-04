4b99d082e42712a28dd6c15c7c14c3d75d17934d
Chrishel09 <chrishelpinto240906@gmail.com>
2025-10-04 12:13:26 +0530

    Fibonacci of a number

---
+++ fib.c
@@ -0,0 +1,19 @@
+// fibonacci using recurssions
+
+#include<stdio.h>
+int fib(int n)
+{
+    if(n==0){
+        return 0;
+    }
+    if(n==1){
+        return 1;
+    }
+   int fibN=fib(n-1)+fib(n-2);
+return fibN;
+}
+int main()
+{
+   printf("%d ",fib(9));
+   return 0;
+    }
